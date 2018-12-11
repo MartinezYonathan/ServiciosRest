eaaac3544f1a002a80f752fa6a035a5b5c349d68
yonathan martinez <j-ma@hotmail.es>
2018-12-11 17:30:44 -0600

    Se agrego datos a .propertis de la BD H2 y se agrego assert a prueba de testCurso

--- src/test/java/mx/edu/uacm/api/test/domain/CursoTest.java
+++ src/test/java/mx/edu/uacm/api/test/domain/CursoTest.java
@@ -3,8 +3,12 @@
  */
 package mx.edu.uacm.api.test.domain;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 //import org.apache.logging.log4j.LogManager;
 //import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -21,34 +25,42 @@ import mx.edu.uacm.api.persistencia.CursoRepository;
  * @author christian
  *
  */
-/*anotaciones*/
-@RunWith(SpringRunner.class)//y se importa
-@SpringBootTest(classes = ServiciosRestApplication.class)//le paso mi clase 
-//con la que voy a comenzar a ejecutar
-//la aplicaciÃ³n en este caso se llama ServiciosRestApplication, se debe importar
+/* anotaciones */
+@RunWith(SpringRunner.class) // y se importa
+@SpringBootTest(classes = ServiciosRestApplication.class) // le paso mi clase
+// con la que voy a comenzar a ejecutar
+// la aplicacion en este caso se llama ServiciosRestApplication, se debe
+// importar
 public class CursoTest {
-	//Estamos utilizando Test Driven Development(TDD Desarrollo guiado por pruebas), nos recomienda que debemos
-	//iniciar desarrollando pruebas
-	//y estamos tratando de 
-	//no repertir las operaciones CRUD
-	//COMENZAMOS CON LA PARTE DE LA BASE DE DATOS
-	private static final Logger log=LogManager.getLogger(CursoTest.class);
-	//private static final Logger log = Logger.get getLog(CursoTest.class);
-	//el logger se importa el de apache
-	@Autowired //utilizamos la inyecciÃ³n de dependencias para inyectar 
-	//la implemetaciÃ³n de la interfaz
-	CursoRepository cursoRepository;// es una interfaz 
-	//el primer test 
-	@Test  //se importa el de junit
-	public void testGuardarCurso()
-	{log.debug("Entrando al metodo testGuardarCurso");
-		
-		Curso curso= new Curso();
+	// Estamos utilizando Test Driven Development(TDD Desarrollo guiado por
+	// pruebas), nos recomienda que debemos
+	// iniciar desarrollando pruebas
+	// y estamos tratando de
+	// no repertir las operaciones CRUD
+	// COMENZAMOS CON LA PARTE DE LA BASE DE DATOS
+	private static final Logger log = LogManager.getLogger(CursoTest.class);
+	// private static final Logger log = Logger.get getLog(CursoTest.class);
+	// el logger se importa el de apache
+	@Autowired // utilizamos la inyeccion de dependencias para inyectar
+	// la implemetacion de la interfaz
+	CursoRepository cursoRepository;// es una interfaz
+	// el primer test
+
+	@Test // se importa el de junit
+	public void testGuardarCurso() {
+		log.debug("-------------------------------------------------------");
+		log.debug("--------------------Entrando al metodo testGuardarCurso");
+
+		Curso curso = new Curso();
 		curso.setNombre("mi primer curso");
 		curso.setDescripcion("curso inicial");
-		
-		//recuerda que estamos usando una BD h2 = en memoria
-		cursoRepository.save(curso);//guardamos un curso en la base de datos
+
+		// recuerda que estamos usando una BD h2 = en memoria
+		Curso cursoReturn = cursoRepository.save(curso);// guardamos un curso en la base de datos
+
+		// Con ester assert verificamos que el objeto retornado no es null
+		// y con esto nos aseguramos que esta funcionado la clase Curso
+		Assert.assertThat(cursoReturn, is(notNullValue()));
 	}
-	
+
 }
