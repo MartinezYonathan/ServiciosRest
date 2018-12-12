df613ac6a62f2baeb491b46faf84b8629be18995
yonathan martinez <j-ma@hotmail.es>
2018-12-11 18:34:29 -0600

    Se agrego la clase Cus

--- src/main/java/mx/edu/uacm/api/services/CursoService.java
+++ src/main/java/mx/edu/uacm/api/services/CursoService.java
@@ -1,7 +1,12 @@
 package mx.edu.uacm.api.services;
 
+import java.util.List;
 import mx.edu.uacm.api.domain.Curso;
 
 public interface CursoService {
 	public Curso guardarCurso(Curso curso);
+	public Curso editarCurso(Curso curso);
+	public Curso borrarCurso(Curso curso);
+	public Curso mostrarCurso(Long id);
+	public List<Curso> mostrarCurso();
 }
