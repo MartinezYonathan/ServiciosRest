cdf1ef302b855005600cc302a6cd65f9536a6a4a
yonathan martinez <j-ma@hotmail.es>
2018-12-11 19:18:31 -0600

    Se crea clace PersonaService y se agregaron metodos de CRUD

---
+++ src/main/java/mx/edu/uacm/api/services/PersonaService.java
@@ -0,0 +1,13 @@
+package mx.edu.uacm.api.services;
+
+import java.util.List;
+
+import mx.edu.uacm.api.domain.Persona;
+
+public interface PersonaService {
+	public Persona guardarPersona(Persona persona);
+	public Persona editaPersona(Persona persona);
+	public String borrarPersona(Long id);
+	public Persona mostrarPersona(Long id);
+	public List<Persona> mostrarPersona();
+}
